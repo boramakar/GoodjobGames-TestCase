a557dad7c84f502e951d13c519954b445131a89e
boramakar <boramakar@gmail.com>
2022-10-22 15:54:11 +0300

    Menu button added to GameScene

---
+++ Assets/01-Scripts/UI/MenuButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    private GameManager _gameManager;
+
+    private void Awake()
+    {
+        _gameManager = GameManager.Instance;
+    }
+
+    public void OnClick()
+    {
+        _gameManager.ChangeScene(SceneType.MenuScene);
+    }
+}
